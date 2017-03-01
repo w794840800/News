e56587d2ae6216f933842d2c929441222a067e49
wanglei <13665675871@qq.com>
2017-03-01 10:14:56 +0800

    add textview test

--- app/src/main/java/comn/example/administrator/news/Main2Activity.java
+++ app/src/main/java/comn/example/administrator/news/Main2Activity.java
@@ -2,6 +2,7 @@ package comn.example.administrator.news;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        TextureView textureView;
     }
 }
