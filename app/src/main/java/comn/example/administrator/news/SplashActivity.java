dab5bf0560bbb14c1808d15dc7832f18804e3ed3
wanglei <13665675871@qq.com>
2017-03-23 15:32:37 +0800

    slpashActivity

--- app/src/main/java/comn/example/administrator/news/SplashActivity.java
+++ app/src/main/java/comn/example/administrator/news/SplashActivity.java
@@ -17,12 +17,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
 import java.util.List;
 
 import comn.example.administrator.news.inter.BlueService;
 import comn.example.administrator.news.jean.douban;
 import comn.example.administrator.news.mvp.view.MainActivity;
+import comn.example.administrator.news.sp.SharedPreferences;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -30,17 +35,20 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Observable;
-import rx.Observer;
-import rx.Scheduler;
+import rx.Subscriber;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 import rx.schedulers.Schedulers;
+import utils.isConnection;
+
 
 public class SplashActivity extends AppCompatActivity {
-ImageView imageView;
+
+    ImageView imageView;
     Button skip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
        // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
@@ -97,6 +105,62 @@ ImageView imageView;
     private void initView() {
         skip= (Button) findViewById(R.id.button_skip);
         imageView= (ImageView) findViewById(R.id.imageView);
+        Subscription observable=Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+         if (isConnection.isNetWork(SplashActivity.this)){
+              OkHttpClient okHttpClient=new OkHttpClient.Builder()
+                      .build();
+                Request request=new Request.Builder()
+                        .url("http://guolin.tech/api/bing_pic")
+                        .build();
+                try {
+                    okhttp3.Response response =okHttpClient.newCall(request)
+                            .execute();
+
+                     android.content.SharedPreferences sharedPreferences = getSharedPreferences("url", MODE_PRIVATE);
+                       android.content.SharedPreferences.Editor editor = sharedPreferences.edit();
+                     String url=response.body().string();
+                     editor.putString("url",url);
+                    editor.commit();
+                       subscriber.onNext(url);
+
+                   } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+             // Log.d("code",response.code()+"");
+                //Toast.makeText(getApplicationContext(),""+response.code(),Toast.LENGTH_SHORT).show();
+//                subscriber.onNext(response.body().string());
+                }
+else {
+             android.content.SharedPreferences   sharedPreferences= getSharedPreferences("url",MODE_PRIVATE);
+             String url=sharedPreferences.getString("url","");
+
+             subscriber.onNext(url);
+         }
+
+            }
+        })
+               .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Glide.with(getApplicationContext()).load(s).into(imageView);
+
+    /*  if (url!=null) {
+           Glide.with(getApplicationContext()).load(url).into(imageView);
+       }*/
+             /*       {
+           Glide.with(getApplicationContext())
+                   .load(s)
+                   .into(imageView);
+
+
+       }*/
+                    }
+                });
 
     }
 
